188e7d10d55d7af507ebb0db272685545ffce1c4
hemanth <applicationcomputer257@gmail.com>
2023-07-02 20:17:30 +0530

    intial commit

---
+++ date.js
@@ -0,0 +1,27 @@
+// Write a JavaScript program to display the current date and time.
+
+let date=new Date();
+
+//current Date
+console.log(date.getDate());
+
+//current month
+console.log(date.getMonth());
+
+//current year
+console.log(date.getFullYear());
+
+// format
+console.log(`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`);
+
+// current hour
+console.log(date.getHours());
+
+// current seconds
+console.log(date.getSeconds());
+
+
+
+
+
+
